46ebe595ef04778b0b0f152b5d4b176a01311baa
Pavan K <23pavank@gmail.com>
2026-03-21 17:19:32 +0000

    handling invalid commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+
+  std::string input;
+
+  std::getline(std::cin, input);
+
+  std::cout << input << ": command not found" << std::endl;
 }
