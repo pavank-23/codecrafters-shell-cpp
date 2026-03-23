db6f846faa8989252c2544497bec354a0eb3b7c7
Pavan K <23pavank@gmail.com>
2026-03-23 10:24:02 +0000

    exit

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,18 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  while (true) {
+  while (true)
+  {
     std::cout << "$ ";
-  
+
     std::string input;
     std::getline(std::cin, input);
+
+    if (input == "exit")
+    {
+      break;
+    }
+
     std::cout << input << ": command not found" << std::endl;
   }
 }
