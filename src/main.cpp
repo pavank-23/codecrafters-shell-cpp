974296269543d17e838843142bbe4e071cd9b031
Pavan K <23pavank@gmail.com>
2026-03-23 10:39:05 +0000

    type

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <set>
 
 int main()
 {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  std::set<std::string> builtin_commands = {"echo", "exit", "type"};
 
   while (true)
   {
@@ -20,7 +23,19 @@ int main()
     }
     else if (input.substr(0, 4) == "echo")
     {
-      std::cout << input.substr(5, input.size() - 5) << std::endl;
+      std::cout << input.substr(5) << std::endl;
+    }
+    else if (input.substr(0, 4) == "type")
+    {
+      std::string arg = input.substr(5);
+      if (builtin_commands.find(arg) != builtin_commands.end())
+      {
+        std::cout << arg << " is a shell builtin" << std::endl;
+      }
+      else
+      {
+        std::cout << arg << ": not found";
+      }
     }
     else
     {
