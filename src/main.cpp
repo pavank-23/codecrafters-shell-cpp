4ee38cba1715c3b3a96de116043d05806ac6ada7
Pavan K <23pavank@gmail.com>
2026-03-23 11:47:13 +0000

    PATH type

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <set>
+#include <cstdlib>
+#include <unordered_map>
+#include <filesystem>
 
 int main()
 {
@@ -9,6 +13,48 @@ int main()
   std::cerr << std::unitbuf;
 
   std::set<std::string> builtin_commands = {"echo", "exit", "type"};
+  std::vector<std::string> paths;
+  const char *env_p = std::getenv("PATH");
+
+  std::string curr = "";
+
+  for (const char *p = env_p; *p != '\0'; ++p)
+  {
+    char c = *p;
+    if (c == ':')
+    {
+      paths.push_back(curr);
+      curr = "";
+    }
+    else
+    {
+      curr += c;
+    }
+  }
+
+  std::unordered_map<std::string, std::string> execs;
+
+  for (std::string p : paths)
+  {
+    for (const auto &entry : std::filesystem::directory_iterator(p))
+    {
+      if (entry.is_directory() == true)
+      {
+        try
+        {
+          for (const auto &subentry : std::filesystem::directory_iterator(entry.path()))
+          {
+            execs[subentry.path().filename()] = subentry.path();
+          }
+        }
+        catch (std::filesystem::__cxx11::filesystem_error)
+        {
+          // std::cerr << "No Permissions to access subentries of: " << entry.path() << std::endl;
+          continue;
+        }
+      }
+    }
+  }
 
   while (true)
   {
@@ -34,7 +80,12 @@ int main()
       }
       else
       {
-        std::cout << arg << ": not found" << std::endl;
+        if (execs.find(arg) != execs.end())
+        {
+          std::cout << arg << " is " << execs[arg] << std::endl;
+        }
+        else
+          std::cout << arg << ": not found" << std::endl;
       }
     }
     else
